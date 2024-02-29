086448d76f28acfb56b0b08a4408a7b7c727e257
id22art <id22art@cs.umu.se>
2024-02-29 08:01:17 +0100

    Added ABout-page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import Home from "./pages/Home"
+import About from "./pages/About"
 
 import Header from "./components/Header"
 import Footer from "./components/Footer"
@@ -9,15 +10,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+      <div>
+          <Router>
+              <Header />
+              <Routes>
+                  <Route path="/" element={<Home />}/>
+              </Routes>
+              <Footer />
+          </Router>
+      </div>
   );
 }
 
